b3a52a3136540de86223d8f0eed300b72ad24947
bruno <bru65pag@gmail.com>
2020-05-16 16:52:42 +0200

    use of gfx library to draw various circles

--- gfx_draw_circle/src/main.rs
+++ gfx_draw_circle/src/main.rs
@@ -14,12 +14,34 @@ pub fn main() {
     let window = video_subsystem.window("sdl2 demo", 800, 600).build().unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
 
+    // draw circle
     canvas.set_draw_color(Color::RGB(255,255,255)); // white
-    canvas.clear(); // clear the canvas with  the color set on the previous line
-
-    canvas.circle(400,300,50,Color::RGB(255,0,0)).unwrap();
+    canvas.clear();
+    canvas.circle(50,50,50,Color::RGB(255,0,0)).unwrap();
     canvas.present();
+    sleep(Duration::from_secs(2));
 
-    sleep(Duration::from_secs(5));
+    // draw anti alias circle
+    canvas.set_draw_color(Color::RGB(255,255,255)); // white
+    canvas.clear();
+    canvas.aa_circle(50,50,50,Color::RGB(255,0,0)).unwrap();
+    canvas.present();
+    sleep(Duration::from_secs(2));
+
+    // draw filled circleColor
+    canvas.set_draw_color(Color::RGB(255,255,255)); // white
+    canvas.clear();
+    canvas.filled_circle(50,50,50,Color::RGB(255,0,0)).unwrap();
+    canvas.present();
+    sleep(Duration::from_secs(2));
+
+    // no straight way to draw a circle with a certain line thickness
+    // instead, drawing two circles
+    canvas.set_draw_color(Color::RGB(255,255,255)); // white
+    canvas.clear();
+    canvas.filled_circle(50,50,50,Color::RGB(255,0,0)).unwrap();
+    canvas.filled_circle(50,50,45,Color::RGB(255,255,255)).unwrap(); 
+    canvas.present();
+    sleep(Duration::from_secs(2));
 
 }
